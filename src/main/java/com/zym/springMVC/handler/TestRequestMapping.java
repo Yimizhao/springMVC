e8ce56d3ec493d5d7191ca0987b53503ae3f5ede
Yimizhao <zhao12051610@163.com>
2018-05-02 14:08:51 +0800

    代码整理

--- src/main/java/com/zym/springMVC/handler/TestRequestMapping.java
+++ src/main/java/com/zym/springMVC/handler/TestRequestMapping.java
@@ -1,12 +1,10 @@
 package com.zym.springMVC.handler;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/springMVC")
 @Controller
